14dd2fe4e2939f1fe52744f602c1753433a6c2e7
KierPalin <45743174+KierPalin@users.noreply.github.com>
2025-10-29 06:50:18 +0000

    main.cpp: reduced mem cost for same crash

--- source/main.cpp
+++ source/main.cpp
@@ -49,7 +49,7 @@ void calculate_expensive_value()
 */
 volatile double *memory_expensive_fn()
 {
-    const size_t nums = 2000;
+    const size_t nums = 800;
     volatile double *src = static_cast<volatile double *>(malloc(nums * sizeof(double)));
 
     for (size_t i = 0; i < nums; i++)
@@ -69,7 +69,7 @@ volatile double *memory_expensive_fn()
 
 void update_loop()
 {
-    const size_t num_fibers_calculating_expensive_value = 8;
+    const size_t num_fibers_calculating_expensive_value = 8; // 6 should prevent hanging
     while (1)
     {
         fiber_sleep(UPDATE_PRIORITY);
