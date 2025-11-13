bf385565ec41ab8faf8fb0f0d5efed48b12ebd49
KierPalin <45743174+KierPalin@users.noreply.github.com>
2025-11-13 14:20:50 +0000

    main.cpp: SSD1306.h include removed, added I2C setup

--- source/main.cpp
+++ source/main.cpp
@@ -1,10 +1,18 @@
 #include "MicroBit.h"
-#include "SSD1306.h"
+#include "CodalFiber.h"
 
 MicroBit uBit;
 
 int main()
 {
     uBit.init();
-    uBit.display.scroll("Hi :)");
+
+    // I2C setup:
+    uBit.io.P0.setPull(PullMode::None);
+    uBit.io.P1.setPull(PullMode::None);
+    uBit.i2c.redirect(uBit.io.P0, uBit.io.P1);
+    uBit.i2c.setFrequency(400000);
+
+    release_fiber(); // Don't exit main
+    return 1;
 }
