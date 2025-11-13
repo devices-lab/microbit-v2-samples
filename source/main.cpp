3d61f2cfa0cb75916128e0ddd2e99b262585cbb7
KierPalin <45743174+KierPalin@users.noreply.github.com>
2025-11-13 14:39:19 +0000

    main.cpp reset, CMakeLists obj commented out

--- source/main.cpp
+++ source/main.cpp
@@ -1,5 +1,4 @@
 #include "MicroBit.h"
-#include "CodalFiber.h"
 
 MicroBit uBit;
 
@@ -7,12 +6,5 @@ int main()
 {
     uBit.init();
 
-    // I2C setup:
-    uBit.io.P0.setPull(PullMode::None);
-    uBit.io.P1.setPull(PullMode::None);
-    uBit.i2c.redirect(uBit.io.P0, uBit.io.P1);
-    uBit.i2c.setFrequency(400000);
-
-    release_fiber(); // Don't exit main
-    return 1;
+    uBit.display.scroll("Steve");
 }
