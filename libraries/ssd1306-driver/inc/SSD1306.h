c9a47a1cf6eea04b034f5d2253d2e92053c3d15e
KierPalin <45743174+KierPalin@users.noreply.github.com>
2025-11-13 19:13:51 +0000

    Added driver + updated header

--- libraries/ssd1306-driver/inc/SSD1306.h
+++ libraries/ssd1306-driver/inc/SSD1306.h
@@ -15,16 +15,13 @@
 #include "MicroBit.h"
 extern MicroBit uBit;
 
-#define OLED_BLACK 0
-#define OLED_WHITE 1
+#define OLED_PIXEL_ON true
+#define OLED_PIXEL_OFF false
 #define OLED_WIDTH 128
 #define OLED_HEIGHT 64
 
-#define OLED_PIXELS_PER_PAGE 8
-#define OLED_NUM_PAGES_PER_COL OLED_HEIGHT / OLED_PIXELS_PER_PAGE
-
 void SSD1306_init();
-void SSD1306_setPixel(uint8_t x, uint8_t y, uint8_t colour);
+void SSD1306_setPixel(uint8_t x, uint8_t y, bool on);
 void SSD1306_clearColumn(uint8_t col);
 
 #endif // !__SD1306_OBF_H
